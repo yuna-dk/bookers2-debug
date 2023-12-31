7eca85acd084b1788d5a0f136f1076158eef8155
yuna-dk <149683910+yuna-dk@users.noreply.github.com>
2023-12-31 01:09:59 +0000

    [Finish] 5

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,17 +1,16 @@
 class SearchesController < ApplicationController
   before_action :authenticate_user!
-  
+
   def search
     @range = params[:range]
     @word = params[:word]
 
     if @range == "User"
       @users = User.looks(params[:search], params[:word])
-      redirect_to search_path
     else
       @books = Book.looks(params[:search], params[:word])
-      render 'search_result'
     end
+    render 'search_result'
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
-  get "/search" => "searches#search"
+  get "search" => "searches#search"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorite, only: [:create, :destroy]
