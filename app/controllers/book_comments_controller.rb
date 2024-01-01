45f69fac82c884c7fbc5ccce70a0a7734d254c1d
yuna-dk <149683910+yuna-dk@users.noreply.github.com>
2024-01-01 02:25:10 +0000

    [Add] 6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,12 +5,12 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
   
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
 
   private
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,5 +1,6 @@
 class FavoritesController < ApplicationController
-
+  before_action :authenticate_user!
+  
   def create
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: @book.id)
