ba75656a86da9cfc8e45f806e3304c3842ed5204
yuna-dk <149683910+yuna-dk@users.noreply.github.com>
2024-01-04 06:43:46 +0000

    [Finish] 6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,14 +1,15 @@
 class BookCommentsController < ApplicationController
-  
+
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.save
     # redirect_back(fallback_location: root_path)
   end
-  
+
   def destroy
+    @book = Book.find(params[:book_id])
     BookComment.find(params[:id]).destroy
     # redirect_back(fallback_location: root_path)
   end
@@ -18,5 +19,5 @@ class BookCommentsController < ApplicationController
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
-  
+
 end
